e50bc575b530a84141d42f616561e1413cef2f5b
Brent Presley <brent.presley@mstc.edu>
2025-09-21 10:29:40 -0500

    Add ASP.NET Core project

---
+++ deploymentfall25/deploymentfall25/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace deploymentfall25.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
